dad86cb03598060ccacfc774a364a2a5f8e1e6b1
ryan heupler & travis crampton <student@epicodus.com>
2015-09-15 15:35:18 -0700

    adds functionality for showing ticket price

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,49 @@
-function Movie(movieTitle, movieTime, viewerAge, movieAge, moviePrice) {
+function Movie(movieTitle, movieTime, viewerAge, movieAge, ticketQuantity) {
   this.movieTitle = movieTitle;
-  this.movieTime = [];
+  this.movieTime = movieTime;
   this.viewerAge = viewerAge;
   this.movieAge = movieAge;
-  this.moviePrice = moviePrice;
+  this.ticketQuantity = ticketQuantity;
 }
-
 $(function() {
-  $("input:button").click(function(){
-    $(".show-movie-info").show();
+  $("form#movies").submit(function(event) {
     event.preventDefault();
-    var inputtedMovieTitle = $(this).val();
-    var newMovie = new Movie(inputtedMovieTitle);
 
-    $("ul#movies").append("<li>Showtimes for: <span class='each-movie'>" + newMovie.movieTitle + "</span></li>");
+    var selectedTitle = $("select#movieTitles option:selected").val();
+    var selectedTime = $("select#movieTimes option:selected").val();
+    var selectedAge = $("select#ticketType option:selected").val();
+    var ticketQuantity = parseInt($("select#ticketQuantity option:selected").val());
+    var newMovie = new Movie(selectedTitle, selectedTime, selectedAge, ticketQuantity);
+
+    var ticketPrice = 8;
+    if (newMovie.movieTitle === ("The Visit") || newMovie.movieTitle === ("Black Mass") || newMovie.movieTitle === ("Straight Outta Compton")) {
+      ticketPrice += 2;
+    }  else {
+      ticketPrice;
+    }
+
+    if (newMovie.movieTime === ("4:45 PM") || newMovie.movieTime === ("6:15 PM") || newMovie.movieTime === ("8:45 PM") || newMovie.movieTime === ("10:05 PM")) {
+      ticketPrice +=2;
+    } else {
+      ticketPrice;
+    }
+
+    if (newMovie.viewerAge === ("adult")) {
+      ticketPrice +=2;
+    } else {
+        ticketPrice;
+      }
+
+    if (ticketQuantity === 2) {
+      ticketPrice *= 2;
+    } else if (ticketQuantity === 3) {
+      ticketPrice *= 3;
+    } else if (ticketQuantity === 4) {
+      ticketPrice *= 4;
+    } else {
+      ticketPrice;
+    }
+
+    $("#display-price").empty().append("<h2><span class='movie-cost'>" + ticketPrice + "</span> Dollars</h2>");
   });
 });
---
+++ spec/specs.js
@@ -0,0 +1,16 @@
+// describe('movie', function() {
+//   it("Saves The Title of the movie", function(){
+//     var testMovie = new Movie()
+//   });
+// });
+// <div class="form-group">
+// <ul>
+// <input type='button' class="form-control movie" value="The Visit"><br>
+// <input type='button' class="form-control movie" value="Black Mass"><br>
+// <input type='button' class="form-control movie" value="Straight Outta Compton"><br>
+// <input type='button' class="form-control movie" value="Mission: Impossible-Rogue Nation"><br>
+// <input type='button' class="form-control movie" value="Everest"><br>
+// <input type='button' class="form-control movie" value="Inside Out"><br>
+// <input type='button' class="form-control movie" value="Trainwreck"><br>
+// </ul>
+// </div>
